daf88048d91fa66213c224eb3dffe9190c42f5a6
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2022-12-07 14:56:36 -0300

    Add files via upload

---
+++ PVDiciembre2022_FloresJose/src/main/java/ar/edu/unju/edm/PvDiciembre2022FloresJoseApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvDiciembre2022FloresJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvDiciembre2022FloresJoseApplication.class, args);
+	}
+
+}
---
+++ PVDiciembre2022_FloresJose/src/main/java/ar/edu/unju/edm/models/Pregunta.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.edm.models;
+
+public class Pregunta {
+
+}
